d311d4c0ac8574a4bc053b480ff1ddc29ff56980
Lau Taarnskov <lau@taarnskov.com>
2017-05-03 09:36:21 -0300

    Allow encoding native Elixir DateTimes (not fully tested)

--- lib/bson/encoder.ex
+++ lib/bson/encoder.ex
@@ -28,6 +28,11 @@ defmodule BSON.Encoder do
   def encode(%BSON.DateTime{utc: utc}) when is_int64(utc),
     do: <<utc::int64>>
 
+  def encode(%DateTime{time_zone: "Etc/UTC"} = datetime) do
+    unix_ms = DateTime.to_unix!(datetime, :milliseconds)
+    <<unix_ms::int64>>
+  end
+
   def encode(%BSON.Regex{pattern: pattern, options: options}),
     do: [cstring(pattern) | cstring(options)]
 
@@ -128,6 +133,7 @@ defmodule BSON.Encoder do
   defp type(%BSON.Binary{}),                do: @type_binary
   defp type(%BSON.ObjectId{}),              do: @type_objectid
   defp type(%BSON.DateTime{}),              do: @type_datetime
+  defp type(%DateTime{}),                   do: @type_datetime
   defp type(%BSON.Regex{}),                 do: @type_regex
   defp type(%BSON.JavaScript{scope: nil}),  do: @type_js
   defp type(%BSON.JavaScript{}),            do: @type_js_scope
